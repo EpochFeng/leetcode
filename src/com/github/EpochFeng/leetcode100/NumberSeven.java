d02de56aca70554667029d26d03fd489b974419d
Feng <1373500643@qq.com>
2026-05-15 10:25:10 +0800

    feat(leetcode): 添加第七题接雨水算法实现
    - 实现了两种不同的接雨水算法解决方案
    - 第一种方法通过逐层计算雨水容量
    - 第二种方法使用左右最大值查找策略
    - 添加了测试用例验证算法正确性
    - 完善了类注释和方法文档说明

---
+++ src/com/github/EpochFeng/leetcode100/NumberSeven.java
@@ -0,0 +1,68 @@
+package com.github.EpochFeng.leetcode100;
+
+/**
+ * @author 冯纪元
+ * @ClassName NumberSeven
+ * @description: TODO
+ * @datetime 2026年 05月 14日 15:24
+ * @version: 1.0
+ */
+public class NumberSeven {
+    public static void main(String[] args) {
+        int[] arry = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int trap = trap1(arry);
+        System.out.println(trap);
+    }
+
+    public static int trap(int[] height) {
+        int maxHeight = 0;
+        for (int i : height) {
+            if (i > maxHeight) {
+                maxHeight = i;
+            }
+        }
+        int ans = 0;
+        for (int j = 1; j <= maxHeight; j++) {
+            boolean isTrue = false;
+            int temp = 0;
+            for (int i = 0; i < height.length; i++) {
+                if (isTrue && height[i] < j) {
+                    temp += 1;
+                }
+                if (height[i] >= j) {
+                    ans += temp;
+                    isTrue = true;
+                    temp = 0;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int trap1(int[] height) {
+        int ans = 0;
+        for (int i = 1; i < height.length - 1; i++) {
+            int temp = height[i];
+            int maxLeft = 0;
+            for (int left = 0; left < i; left++) {
+                if (height[left] > maxLeft) {
+                    maxLeft = height[left];
+                }
+            }
+            int maxRight = 0;
+            for (int right = i + 1; right < height.length; right++) {
+                if (height[right] > maxRight) {
+                    maxRight = height[right];
+                }
+            }
+            if (temp < maxLeft && temp < maxRight) {
+                int min = Math.min(maxLeft, maxRight);
+                ans += min - temp;
+            }
+        }
+        return ans;
+    }
+
+
+
+}
