e5d3c55828851e259d1a41fae35866896ef4d847
Feng <1373500643@qq.com>
2026-05-18 09:37:52 +0800

    feat(NumberTen): 添加子数组和算法实现
    - 引入HashMap和Map用于优化算法实现
    - 添加main方法用于测试功能
    - 实现subarraySum1静态方法，使用前缀和与哈希表优化子数组和计算
    - 通过前缀和数组和哈希表降低时间复杂度
    - 添加测试用例验证算法正确性

--- src/com/github/EpochFeng/leetcode100/NumberTen.java
+++ src/com/github/EpochFeng/leetcode100/NumberTen.java
@@ -1,5 +1,8 @@
 package com.github.EpochFeng.leetcode100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author 冯纪元
  * @ClassName NumberTen
@@ -8,6 +11,9 @@ package com.github.EpochFeng.leetcode100;
  * @version: 1.0
  */
 public class NumberTen {
+    public static void main(String[] args) {
+        int i = subarraySum1(new int[]{1,2,3}, 3);
+    }
     public int subarraySum(int[] nums, int k) {
         int ans = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -25,5 +31,22 @@ public class NumberTen {
         return ans;
     }
 
+    public static int subarraySum1(int[] nums, int k) {
+        int ans = 0;
+        int[] arry = new int[nums.length+1];
+        arry[0]=0;
+        for (int i = 0; i < nums.length; i++) {
+            arry[i+1] = arry[i]+nums[i];
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i : arry) {
+            int i1 = i - k;
+            if (map.containsKey(i1)){
+                ans+=map.get(i1);
+            }
+           map.put(i,map.getOrDefault(i,0)+1);
+        }
+        return ans;
+    }
 
 }
