673636d785f614b0f97a7e998235878d6e0e0935
Feng <1373500643@qq.com>
2026-05-21 10:39:58 +0800

    feat(leetcode): 添加第16题数组乘积除自身算法实现
    - 实现了productExceptSelf方法计算数组除自身外其他元素乘积
    - 使用左右两遍遍历算法避免除法运算
    - 创建left数组存储左侧元素乘积
    - 创建right数组存储右侧元素乘积
    - 最终将左右乘积相乘得到结果
    - 时间复杂度O(n)，空间复杂度O(1)

---
+++ src/com/github/EpochFeng/leetcode100/NumberSixteen.java
@@ -0,0 +1,27 @@
+package com.github.EpochFeng.leetcode100;
+
+/**
+ * @author 冯纪元
+ * @ClassName NumberSixteen
+ * @description: TODO
+ * @datetime 2026年 05月 21日 9:53
+ * @version: 1.0
+ */
+public class NumberSixteen {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = nums[i-1]*left[i-1];
+        }
+        int[] right = new int[nums.length];
+        right[nums.length-1] = 1;
+        for (int i = nums.length-2; i >=0; i--) {
+            right[i] = right[i+1]*nums[i+1];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = left[i]*right[i];
+        }
+        return nums;
+    }
+}
